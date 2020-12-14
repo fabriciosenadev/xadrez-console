f847e742ac883bea137e730ecf2fd5515de6beda
Fabricio Sena <fabricior.sena@outlook.com>
2020-12-14 01:06:48 -0300

    Criando a classe PosicaoXadrez

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -10,13 +10,19 @@ namespace xadrez_console
         {
             try
             {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                //Tabuleiro tab = new Tabuleiro(8, 8);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                //tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                //tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                //tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
-                Tela.imprimirTabuleiro(tab);
+                //Tela.imprimirTabuleiro(tab);
+
+                PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+
+                Console.WriteLine(pos);
+
+                Console.WriteLine(pos.toPosicao());
             }
             catch (TabuleiroException e)
             {
---
+++ xadrez-console/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tabuleiro;
+
+namespace xadrez
+{
+    class PosicaoXadrez
+    {// posição para exibição no tabuleiro de xadrez
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        // converte a posição do tabuleiro para uma posição de matriz
+        public Posicao toPosicao()
+        {
+            // numero da linha, código da coluna
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}{1}", coluna, linha);
+        }
+    }
+}
