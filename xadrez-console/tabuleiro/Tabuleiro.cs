6f250da6b4a6fe41cb777fb7e5eb4dec0f7779a3
Fabricio Sena <fabricior.sena@outlook.com>
2020-12-14 00:00:20 -0300

    Comecando a implementar o tabuleiro

---
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        public Peca[,] pecas; // matriz privativa
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            // this indica a prop quando o parametro tem a mesma nomenclatura
+            this.linhas = linhas; 
+            this.colunas = colunas;
+
+            pecas = new Peca[linhas, colunas]; //inicia com os argumentos informados
+        }
+    }
+}
